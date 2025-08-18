fab00c3919ada0888b690c9ab91c5581fe3a9542
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-18 23:58:57 +1000

    (chore) Basic doco structure

--- Triggaz.tsx
+++ Triggaz.tsx
@@ -4,11 +4,13 @@ import MidiSelector from '../components/MidiSelector';
 const Triggaz: React.FC = () => {
     return (
         <div style={{ padding: '2em' }}>
-            WebMIDI Triggaz midi note/cc pattern detection Editor Tool.<br />
+            <h1>Triggaz midi note/cc pattern detection tool</h1>
             <p className="sunken-paragraph">
                 Triggaz are a bit like Emanators in reverse, they look for patterns in the live performance and when a
                 close enough match has been made it triggers an emanator, or a MIDI CC event or Ableton Live mapped
                 parameter and so on</p>
+            <p className="sunken-paragraph">
+                Triggaz detection pattern browser/gallery and web based pattern learning tools are coming here soon...</p>
 
             <div style={{ margin: '20px 0' }}>
                 <div>
