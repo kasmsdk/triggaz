f96672e9b2bbb4fa0a03963867f50c8b33d86122
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-19 13:06:57 +1000

    (chore) UI tweaks

--- Triggaz.tsx
+++ Triggaz.tsx
@@ -5,18 +5,18 @@ const Triggaz: React.FC = () => {
     return (
         <div className="kasm-landing-container">
             <h1>Triggaz midi note/cc pattern detection tool</h1>
-            <p className="sunken-paragraph">
+            <p>
                 Triggaz are a bit like Emanators in reverse, they look for patterns in the live performance and when a
                 close enough match has been made it triggers an emanator, or a MIDI CC event or Ableton Live mapped
                 parameter and so on</p>
-            <p className="sunken-paragraph">
+            <p>
                 Triggaz detection pattern browser/gallery and web based pattern learning tools are coming here soon...</p>
 
             <div style={{ margin: '20px 0' }}>
-                <div>
+                <p>
                     Connect to your MIDI device... (you might need a <a href="https://help.ableton.com/hc/en-us/articles/209774225-Setting-up-a-virtual-MIDI-bus" target="_blank">virtual MIDI bus</a>)
                     <MidiSelector />
-                </div>
+                </p>
             </div>
         </div>
     );
